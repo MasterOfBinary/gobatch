e610c0273446f67536b6ed743fe9a83f4cc80c27
Vaughn Friesen <vdfriesen@vaughnfriesen.com>
2017-02-01 03:16:33 +0000

    Add BatchBuilder

--- source/channel_test.go
+++ source/channel_test.go
@@ -9,73 +9,71 @@ import (
 )
 
 func TestChannelSource_Read(t *testing.T) {
-	t.Run("buffered channel", func(t *testing.T) {
-		t.Parallel()
+	t.Parallel()
 
-		size := 10
-		ctx, cancel := context.WithCancel(context.Background())
-		defer cancel()
+	size := 10
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-		t.Run("read", func(t *testing.T) {
-			var wg sync.WaitGroup
+	t.Run("read", func(t *testing.T) {
+		var wg sync.WaitGroup
 
-			ch := make(chan interface{}, size)
-			defer close(ch)
+		ch := make(chan interface{}, size)
+		defer close(ch)
 
-			s := Channel(ch)
+		s := Channel(ch)
 
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				values, err := s.Read(ctx)
-				assert.Nil(t, err)
-				assert.NotEmpty(t, values)
-				assert.EqualValues(t, 1, values[0])
-			}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			values, err := s.Read(ctx)
+			assert.Nil(t, err)
+			assert.NotEmpty(t, values)
+			assert.EqualValues(t, 1, values[0])
+		}()
 
-			ch <- 1
-			wg.Wait()
-		})
+		ch <- 1
+		wg.Wait()
+	})
 
-		t.Run("context done", func(t *testing.T) {
-			var wg sync.WaitGroup
+	t.Run("context done", func(t *testing.T) {
+		var wg sync.WaitGroup
 
-			ch := make(chan interface{}, size)
-			defer close(ch)
+		ch := make(chan interface{}, size)
+		defer close(ch)
 
-			s := Channel(ch)
+		s := Channel(ch)
 
-			ctx, cancel := context.WithCancel(ctx)
+		ctx, cancel := context.WithCancel(ctx)
 
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				values, err := s.Read(ctx)
-				assert.Nil(t, err)
-				assert.Empty(t, values)
-			}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			values, err := s.Read(ctx)
+			assert.Nil(t, err)
+			assert.Empty(t, values)
+		}()
 
-			cancel()
-			wg.Wait()
-		})
+		cancel()
+		wg.Wait()
+	})
 
-		t.Run("close", func(t *testing.T) {
-			var wg sync.WaitGroup
+	t.Run("close", func(t *testing.T) {
+		var wg sync.WaitGroup
 
-			ch := make(chan interface{}, size)
+		ch := make(chan interface{}, size)
 
-			s := Channel(ch)
+		s := Channel(ch)
 
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				values, err := s.Read(ctx)
-				assert.Nil(t, err)
-				assert.Empty(t, values)
-			}()
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			values, err := s.Read(ctx)
+			assert.Nil(t, err)
+			assert.Empty(t, values)
+		}()
 
-			close(ch)
-			wg.Wait()
-		})
+		close(ch)
+		wg.Wait()
 	})
 }
