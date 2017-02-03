c1fe7885f218dc27358a1eb438129f930d0c5694
Vaughn Friesen <vdfriesen@vaughnfriesen.com>
2017-02-03 17:04:02 +0800

    Fix go generate and add mocks

--- source/source.go
+++ source/source.go
@@ -6,4 +6,4 @@ type Source interface {
 	Read(ctx context.Context, items chan<- interface{}, errs chan<- error)
 }
 
-// go:generate mockery -name=Source -inpkg -case=underscore
+//go:generate mockery -name=Source -inpkg -case=underscore
