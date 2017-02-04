d1a6818ed587686c7fe18839014b13b4318a3624
Vaughn Friesen <vdfriesen@vaughnfriesen.com>
2017-02-04 20:30:37 +0800

    Add tests for New

--- batch_test.go
+++ batch_test.go
@@ -3,6 +3,7 @@ package gobatch
 import (
 	"errors"
 	"testing"
+	"time"
 )
 
 func TestMust(t *testing.T) {
@@ -24,5 +25,84 @@ func TestMust(t *testing.T) {
 
 	if !panics {
 		t.Error("Must(batch, err) doesn't panic")
+	}
+}
+
+func TestNew(t *testing.T) {
+	tests := []struct {
+		name    string
+		config  *BatchConfig
+		wantErr bool
+	}{
+		{
+			name:    "nil config",
+			config:  nil,
+			wantErr: false,
+		},
+		{
+			name:    "empty config",
+			config:  &BatchConfig{},
+			wantErr: false,
+		},
+		{
+			name: "good config",
+			config: &BatchConfig{
+				MinItems:        5,
+				MaxItems:        10,
+				MinTime:         time.Second,
+				MaxTime:         2 * time.Second,
+				ReadConcurrency: 5,
+			},
+			wantErr: false,
+		},
+		{
+			name: "min time only",
+			config: &BatchConfig{
+				MinTime: time.Second,
+			},
+			wantErr: false,
+		},
+		{
+			name: "min items only",
+			config: &BatchConfig{
+				MinItems: 5,
+			},
+			wantErr: false,
+		},
+		{
+			name: "bad items",
+			config: &BatchConfig{
+				MinItems: 10,
+				MaxItems: 5,
+			},
+			wantErr: true,
+		},
+		{
+			name: "bad times",
+			config: &BatchConfig{
+				MinTime: 2 * time.Second,
+				MaxTime: time.Second,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			batch, err := New(test.config)
+			if test.wantErr && err == nil {
+				t.Error("New(config) returns nil error, want not nil")
+			} else if !test.wantErr {
+				if err != nil {
+					t.Errorf("New(config) returns error %v, want nil", err)
+				}
+				if batch == nil {
+					t.Error("New(config) returns nil batch, want not nil")
+				}
+			}
+		})
 	}
 }
