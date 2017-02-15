7b7b5bfc2cf66eb10422baece2a7459a30df2da9
Vaughn Friesen <vdfriesen@vaughnfriesen.com>
2017-02-16 00:16:47 +0800

    Add JSON config

--- batch/config.go
+++ batch/config.go
@@ -19,7 +19,7 @@ type ConfigValues struct {
 	// before processing items. The exception to this is if a max number
 	// of items was specified and that number is reached before MinTime;
 	// in that case those items will be processed right away.
-	MinTime time.Duration
+	MinTime time.Duration `json:"minTime"`
 
 	// MinItems specifies that a minimum number of items should be
 	// processed at a time. Items will not be processed until MinItems
@@ -27,17 +27,17 @@ type ConfigValues struct {
 	// is specified and that time is reached before the minimum number of
 	// items is available, or if all items have been read and are ready
 	// to process.
-	MinItems uint64
+	MinItems uint64 `json:"minItems"`
 
 	// MaxTime specifies that a maximum amount of time should pass before
 	// processing. Once that time has been reached, items will be processed
 	// whether or not MinItems items are available.
-	MaxTime time.Duration
+	MaxTime time.Duration `json:"maxTime"`
 
 	// MaxItems specifies that a maximum number of items should be available
 	// before processing. Once that number of items is available, they will
 	// be processed whether or not MinTime has been reached.
-	MaxItems uint64
+	MaxItems uint64 `json:"maxItems"`
 }
 
 // ConstantConfig returns a Config with constant values. If values
