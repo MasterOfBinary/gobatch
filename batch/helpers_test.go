4f5505c00ce6cdc9563f330bebbce167b31a43de
Vaughn Friesen <vdfriesen@vaughnfriesen.com>
2017-02-18 16:48:00 +0800

    Add test for NextItem

--- batch/helpers_test.go
+++ batch/helpers_test.go
@@ -7,6 +7,28 @@ import (
 	"time"
 )
 
+func TestNextItem(t *testing.T) {
+	ch := make(chan *Item)
+
+	go func() {
+		ch <- &Item{
+			id: 100,
+		}
+	}()
+
+	ps := &pipelineStage{
+		in: ch,
+	}
+
+	next := NextItem(ps, 200)
+	if next.id != 100 {
+		t.Errorf("next.id == %v, want 100", next.id)
+	}
+	if next.item != 200 {
+		t.Errorf("next.item == %v, want 200", next.item)
+	}
+}
+
 func TestIgnoreErrors(t *testing.T) {
 	done := make(chan struct{})
 	errs := make(chan error)
