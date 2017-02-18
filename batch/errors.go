6d136582349d37c6e5d7073f2f4a38eb7a543ba3
Vaughn Friesen <vdfriesen@vaughnfriesen.com>
2017-02-18 16:43:36 +0800

    Move IgnoreErrors to helpers.go

--- batch/errors.go
+++ batch/errors.go
@@ -1,25 +1,6 @@
 package batch
 
-// IgnoreErrors starts a goroutine that reads errors from errs but ignores them.
-// It can be used with Batch.Go if errors aren't needed. Since the error channel
-// is unbuffered, one cannot just throw away the error channel like this:
-//
-//    // NOTE: bad - this can cause a deadlock!
-//    _ = batch.Go(ctx, p, s)
-//
-// Instead, IgnoreErrors can be used to safely throw away all errors:
-//
-//    IgnoreErrors(batch.Go(ctx, p, s))
-func IgnoreErrors(errs <-chan error) {
-	// nil channels always block, so check for nil first to avoid a goroutine
-	// leak
-	if errs != nil {
-		go func() {
-			for _ = range errs {
-			}
-		}()
-	}
-}
+
 
 // BatchError is a wrapped error message returned on the error channel.
 type BatchError interface {
