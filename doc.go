776a1f0f674410e14f988576564690dfe192a39a
Vaughn Friesen <vdfriesen@vaughnfriesen.com>
2025-05-17 13:18:53 +0800

    Rename gobatch.go to doc.go and update documentation (#26)

---
+++ doc.go
@@ -0,0 +1,10 @@
+// Package gobatch is the root of the GoBatch library.
+//
+// It consists of three subpackages:
+//
+// - batch: The core batching engine for building processing pipelines.
+// - processor: Several Processor implementations for common operations.
+// - source: Source implementations for ingesting data from various origins.
+//
+// See the README.md for an overview of how these pieces fit together.
+package gobatch
